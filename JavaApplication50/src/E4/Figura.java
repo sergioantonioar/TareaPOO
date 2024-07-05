e266151c6d1d8960cc2a110e8f4c02c65d044855
Sergio <111243440+sergioantonioar@users.noreply.github.com>
2024-07-05 01:14:58 -0500

    Agregando Prob3 y Prob4 mitad

---
+++ JavaApplication50/src/E4/Figura.java
@@ -0,0 +1,13 @@
+package E4;
+
+public abstract class Figura {
+
+    public abstract double area();
+
+    public abstract double perimetro();
+
+    public abstract String mostrarFigura();
+    
+    public abstract String mostrarReporte();
+
+}
